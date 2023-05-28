670cf625f25751c06699921baeca161a5bbc850e
muchiri08 <mbogokennedy08@gmail.com>
2023-05-28 13:37:37 +0300

    add entity user

---
+++ src/main/java/com/muchiri/flyway/entity/User.java
@@ -0,0 +1,38 @@
+package com.muchiri.flyway.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    String id;
+    String user_name;
+    String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
